81e00a2587df6a54da6eaaf569fca28da35df26b
Michał Solarz <michalsolarz83@gmail.com>
2015-03-08 10:32:55 +0100

    HelloController

---
+++ src/AppBundle/Controller/HelloController.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: bezimienny
+ * Date: 08.03.15
+ * Time: 10:25
+ */
+
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+
+class HelloController extends Controller {
+
+    /**
+     * @Route("/hello", name="hello")
+     */
+    public function indexAction(){
+
+    }
+}
