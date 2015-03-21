c0da200cc8e568b039d69f848fee4d98252b734e
Michał Solarz <michalsolarz83@gmail.com>
2015-03-21 11:25:38 +0100

    update SecurityController

--- src/AppBundle/Controller/SecurityController.php
+++ src/AppBundle/Controller/SecurityController.php
@@ -13,7 +13,22 @@ class SecurityController extends Controller
      */
     public function indexAction()
     {
-        return $this->render('security/login.html.twig');
+        $authenticationUtils = $this->get('security.authentication_utils');
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render(
+            'security/login.html.twig',
+            array(
+                // last username entered by the user
+                'last_username' => $lastUsername,
+                'error'         => $error,
+            )
+        );
     }
 
 
